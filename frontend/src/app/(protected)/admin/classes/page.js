397082bd9e0fb5398f9f12d9f342a621d08e18b8
najmul-islam <najmulislam519@gmail.com>
2024-12-03 00:16:07 +0600

    remove trycach

--- frontend/src/app/(protected)/admin/classes/page.js
+++ frontend/src/app/(protected)/admin/classes/page.js
@@ -3,20 +3,15 @@ import { Button } from "flowbite-react";
 import Link from "next/link";
 
 const getAllClass = async () => {
-  try {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/classes`, {
-      next: { revalidate: 10 },
-    });
-    if (!response.ok) {
-      throw new Error("Failed to fetch classes");
-    }
-    const data = response.json();
-
-    return data;
-  } catch (error) {
-    console.error("Error fetching classes:", error);
-    return [];
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/classes`, {
+    next: { revalidate: 10 },
+  });
+  if (!response.ok) {
+    throw new Error("Failed to fetch classes");
   }
+  const data = response.json();
+
+  return data;
 };
 
 const ClassesPage = async () => {
